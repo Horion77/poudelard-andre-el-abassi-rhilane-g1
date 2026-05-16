5108fd02aed3c94f174b4d55b01ec64909744b7b
basma rhilane <basma.rhilane@esme.fr>
2026-05-16 22:08:50 +0200

    maison-point

--- univers/maison.py
+++ univers/maison.py
@@ -1,1 +1,10 @@
 # maison.py
+from utils.input_utils import demander_choix
+
+
+def actualiser_points_maison(maisons, nom_maison, points):
+    if nom_maison in maisons:
+        maisons[nom_maison] = maisons[nom_maison] + points
+        print(nom_maison + " : " + str(points) + " points ajoutés. Total : " + str(maisons[nom_maison]) + " points.")
+    else:
+        print("Maison introuvable : " + nom_maison)
