6a0491255dfaec5e4ea495c3012cab55814d8ab4
basma rhilane <basma.rhilane@esme.fr>
2026-05-16 22:09:50 +0200

    def-maison-gagnante

--- univers/maison.py
+++ univers/maison.py
@@ -8,3 +8,19 @@ def actualiser_points_maison(maisons, nom_maison, points):
         print(nom_maison + " : " + str(points) + " points ajoutés. Total : " + str(maisons[nom_maison]) + " points.")
     else:
         print("Maison introuvable : " + nom_maison)
+def afficher_maison_gagnante(maisons):
+    score_max = 0
+    for maison in maisons:
+        if maisons[maison] > score_max:
+            score_max = maisons[maison]
+
+    gagnantes = []
+    for maison in maisons:
+        if maisons[maison] == score_max:
+            gagnantes.append(maison)
+
+    if len(gagnantes) == 1:
+        print("La maison gagnante est " + gagnantes[0] + " avec " + str(score_max) + " points !")
+    else:
+        print("Egalite entre : " + ", ".join(gagnantes) + " avec " + str(score_max) + " points !")
+
